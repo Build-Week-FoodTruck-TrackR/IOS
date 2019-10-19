2f509ef7ef53a05507efdd18b2ccd3048cdecd7c
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-20 04:48:20 +0800

    Add second storyboard, add map, add searchbar

---
+++ FoodTruck TrackR/FoodTruck TrackR/View Controllers/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  FoodTruck TrackR
+//
+//  Created by Jordan Christensen on 10/20/19.
+//  Copyright © 2019 FoodTruckTrackR. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var foodTruckSearchBar: UISearchBar!
+    @IBOutlet weak var foodTruckMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
