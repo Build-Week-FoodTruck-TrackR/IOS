8a5f980743c2a3c0174341e56753d648256fb4a9
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-20 12:12:46 +0800

    Add login page, get current location, add prompt for location, etc.

--- FoodTruck TrackR/FoodTruck TrackR/View Controllers/MapViewController.swift
+++ FoodTruck TrackR/FoodTruck TrackR/View Controllers/MapViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapViewController: UIViewController {
     
     @IBOutlet weak var foodTruckSearchBar: UISearchBar!
     @IBOutlet weak var foodTruckMapView: MKMapView!
     
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupViews()
+        checkLocationServices()
     }
     
     private func setupViews() {
@@ -34,6 +38,53 @@ class MapViewController: UIViewController {
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.text]
     }
     
+    private func setupLocationManager() { // Housekeeping
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // We want the best accuracy
+    }
+    
+    private func centerViewOnUser() { // Grabs users current location and centers map onto location with 10,000 meters in each direction as padding
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion(center: location, latitudinalMeters: 10_000, longitudinalMeters: 10_000)
+            foodTruckMapView.setRegion(region, animated: true)
+        } else {
+            NSLog("User appears to not have connection.")
+        }
+    }
+    
+    private func checkLocationServices() { // Check to make sure Location Services are active
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+            locationManager.startUpdatingLocation()
+        } else { // Alert user if Location Services are disabled and instruct on how to fix
+            let alert = UIAlertController(title: "Location Services Disabled", message: "It looks like location services is disabled. Please enable it in settings.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func checkLocationAuthorization() { // Check to see if user has authorized location tracking in app
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedAlways, .authorizedWhenInUse: // "Allow"
+            foodTruckMapView.showsUserLocation = true // Only show location if user has allowed location tracking
+            centerViewOnUser()
+        case .denied: // "Don't Allow"
+            let alert = UIAlertController(title: "Location Permissions Disabled", message: "It looks like location permission are disabled. Please enable them in settings.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        case .notDetermined: // User has not yet viewed location tracking prompt
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted: // User has parental controls on
+            let alert = UIAlertController(title: "Location is Restricted", message: "It looks like location permission are restricted!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        @unknown default: // Deal with any other case
+            NSLog("Location services/permission status unknown. Please update to latest version of the app!")
+            break
+        }
+    }
+    
 
     /*
     // MARK: - Navigation
@@ -45,4 +96,19 @@ class MapViewController: UIViewController {
     }
     */
 
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Periodically updates to always show user their current location
+        guard let location = locations.last else { return }
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 10_000, longitudinalMeters: 10_000)
+        foodTruckMapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // Checks the auth status if it was changed
+        checkLocationAuthorization()
+    }
 }
