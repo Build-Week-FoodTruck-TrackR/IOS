90382afee4edd77dae6c2fc368af27abd5312593
Percy <percival.ngan@gmail.com>
2019-10-22 15:35:36 -0500

    Changed the code for VendorController and ConsumerController

--- FoodTruck TrackR/FoodTruck TrackR/Model Controllers/VendorController.swift
+++ FoodTruck TrackR/FoodTruck TrackR/Model Controllers/VendorController.swift
@@ -8,274 +8,136 @@
 
 import Foundation
 
-enum HTTPMethod: String {
-    case get = "GET"
-    case put = "PUT"
-    case post = "POST"
-    case delete = "DELETE"
-}
+class VendorController {
+	var user: Any?
+	var token: String?
 
-enum NetworkError: Error {
-    case encodingError
-    case badResponse
-    case otherError(Error)
-    case noData
-    case badDecode
-    case noAuth
-    case invalidInput
-}
+	static let shared = VendorController()
 
-let baseURL = URL(string: "https://food-truck-finder-rj.herokuapp.com/")!
+	func register(user: VendorSignup, completion: @escaping(NetworkError?) -> Void) {
+		let requestURL = baseURL
+			.appendingPathComponent("api")
+			.appendingPathComponent("register")
+		var request = URLRequest(url: requestURL)
+		request.httpMethod = HTTPMethod.post.rawValue
+		request.setValue("application/json", forHTTPHeaderField: "Content-Type")
 
-class UserController {
-    var user: Any?
-    var token: String?
+		let jsonEncoder = JSONEncoder()
+		do {
+			request.httpBody = try jsonEncoder.encode(user)
+		} catch {
+			print("Error encoding user: \(error)")
+			completion(.encodingError)
+			return
+		}
 
-    static let shared = UserController()
+		URLSession.shared.dataTask(with: request) { (data, response, error) in
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 201 {
+				print(response.statusCode)
+				completion(.badResponse)
+				return
+			}
 
-    func register(user: VendorSignup, completion: @escaping(NetworkError?) -> Void) {
-        let requestURL = baseURL
-                        .appendingPathComponent("api")
-                        .appendingPathComponent("register")
-        var request = URLRequest(url: requestURL)
-        request.httpMethod = HTTPMethod.post.rawValue
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+			if let error = error {
+				NSLog("Error signing up: \(error)")
+				completion(.otherError(error))
+				return
+			}
 
-        let jsonEncoder = JSONEncoder()
-        do {
-            request.httpBody = try jsonEncoder.encode(user)
-        } catch {
-            print("Error encoding user: \(error)")
-            completion(.encodingError)
-            return
-        }
+			guard let data = data else {
+				completion(.noData)
+				return
+			}
 
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let response = response as? HTTPURLResponse,
-                response.statusCode != 201 {
-                print(response.statusCode)
-                completion(.badResponse)
-                return
-            }
+			do {
+				let result = try JSONDecoder().decode(ReturnedVendor.self, from: data)
+				self.token = result.token
+			} catch {
+				NSLog("Could not decode object: \(error)")
+				completion(.badDecode)
+			}
 
-            if let error = error {
-                NSLog("Error signing up: \(error)")
-                completion(.otherError(error))
-                return
-            }
+			completion(nil)
+		}.resume()
+	}
 
-            guard let data = data else {
-                completion(.noData)
-                return
-            }
+	func logIn(user: VendorLogin, completion: @escaping(NetworkError?) -> Void) {
 
-            do {
-                let result = try JSONDecoder().decode(ReturnedVendor.self, from: data)
-                self.token = result.token
-            } catch {
-                NSLog("Could not decode object: \(error)")
-                completion(.badDecode)
-            }
+		let requestURL = baseURL.appendingPathComponent(user.username)
 
-            completion(nil)
-        }.resume()
-    }
+		var urlComponents = URLComponents(url: requestURL, resolvingAgainstBaseURL: true)
+		let grantTypeQuery = URLQueryItem(name: "grant_type", value: "password")
+		let usernameQuery = URLQueryItem(name: "username", value: "\(user.username)")
+		let passwordQuery = URLQueryItem(name: "password", value: "\(user.password)")
+		urlComponents?.queryItems = [grantTypeQuery, usernameQuery, passwordQuery]
 
-    func register(user: ConsumerSignup, completion: @escaping(NetworkError?) -> Void) {
-        let requestURL = baseURL
-                        .appendingPathComponent("api")
-                        .appendingPathComponent("register")
+		guard let url = urlComponents?.url else {
+			completion(.invalidInput)
+			return
+		}
+		var request = URLRequest(url: url)
+		request.httpMethod = HTTPMethod.post.rawValue
+		request.setValue("basic bGFtYmRhLWNsaWVudDpsYW1iZGEtc2VjcmV0", forHTTPHeaderField: "Authorization")
+		request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
 
-        var request = URLRequest(url: requestURL)
-        request.httpMethod = HTTPMethod.post.rawValue
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+		// request.httpBody = componentString?.data(using: .utf8)
 
-        let jsonEncoder = JSONEncoder()
-        do {
-            request.httpBody = try jsonEncoder.encode(user)
-        } catch {
-            print("Error encoding user: \(error)")
-            completion(.encodingError)
-            return
-        }
+		let jsonEncoder = JSONEncoder()
+		do {
+			let userData = try jsonEncoder.encode(user)
+			request.httpBody = userData
+		} catch {
+			NSLog("Error encoding userData: \(error)")
+		}
 
-        URLSession.shared.dataTask(with: request) { (data, response, error) in
-            if let response = response as? HTTPURLResponse,
-                response.statusCode != 201 {
-                print(response.statusCode)
-                completion(.badResponse)
-                return
-            }
+		URLSession.shared.dataTask(with: request) { (data, response, error) in
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 200 {
+				NSLog("Response status code is not 200. Status code: \(response.statusCode)")
+			}
 
-            if let error = error {
-                NSLog("Error signing up: \(error)")
-                completion(.otherError(error))
-                return
-            }
+			if let error = error {
+				NSLog("Error verifying user: \(error)")
+				completion(.otherError(error))
+				return
+			}
 
-            guard let data = data else {
-                completion(.noData)
-                return
-            }
+			guard let data = data else {
+				NSLog("No data returned from data task")
+				completion(.noData)
+				return
+			}
 
-            do {
-                let result = try JSONDecoder().decode(ReturnedConsumer.self, from: data)
-                self.token = result.token
-            } catch {
-                NSLog("Could not decode object: \(error)")
-                completion(.badDecode)
-            }
+			let jsonDecoder = JSONDecoder()
+			do {
+				let result = try jsonDecoder.decode(VendorRepresentation.self, from: data)
+				self.token = result.password
+				self.user = user
+				// TODO: Create new Background thread
+				let context = CoreDataStack.shared.mainContext
 
-            completion(nil)
-        }.resume()
-    }
+				context.performAndWait {
+					_ = Vendor(user: result)
+				}
 
-    func logIn(user: Any, completion: @escaping(NetworkError?) -> Void) {
+				self.saveToPersistentStore()
+				if self.token != nil {
+					completion(nil)
+				}
+			} catch {
+				NSLog("Error decoding data/token: \(error)")
+				completion(.badDecode)
+				return
+			}
+		}.resume()
+	}
 
-        if let user = user as? VendorLogin {
-            let requestURL = baseURL.appendingPathComponent(user.username)
-
-            var urlComponents = URLComponents(url: requestURL, resolvingAgainstBaseURL: true)
-            let grantTypeQuery = URLQueryItem(name: "grant_type", value: "password")
-            let usernameQuery = URLQueryItem(name: "username", value: "\(user.username)")
-            let passwordQuery = URLQueryItem(name: "password", value: "\(user.password)")
-            urlComponents?.queryItems = [grantTypeQuery, usernameQuery, passwordQuery]
-
-            guard let url = urlComponents?.url else {
-                completion(.invalidInput)
-                return
-            }
-            var request = URLRequest(url: url)
-            request.httpMethod = HTTPMethod.post.rawValue
-            request.setValue("basic bGFtYmRhLWNsaWVudDpsYW1iZGEtc2VjcmV0", forHTTPHeaderField: "Authorization")
-            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-
-            // request.httpBody = componentString?.data(using: .utf8)
-
-            let jsonEncoder = JSONEncoder()
-            do {
-                let userData = try jsonEncoder.encode(user)
-                request.httpBody = userData
-            } catch {
-                NSLog("Error encoding userData: \(error)")
-            }
-
-            URLSession.shared.dataTask(with: request) { (data, response, error) in
-                if let response = response as? HTTPURLResponse,
-                    response.statusCode != 200 {
-                    NSLog("Response status code is not 200. Status code: \(response.statusCode)")
-                }
-
-                if let error = error {
-                    NSLog("Error verifying user: \(error)")
-                    completion(.otherError(error))
-                    return
-                }
-
-                guard let data = data else {
-                    NSLog("No data returned from data task")
-                    completion(.noData)
-                    return
-                }
-
-                let jsonDecoder = JSONDecoder()
-                do {
-                    let result = try jsonDecoder.decode(VendorRepresentation.self, from: data)
-                    self.token = result.password
-                    self.user = user
-                    // TODO: Create new Background thread
-                    let context = CoreDataStack.shared.mainContext
-
-                    context.performAndWait {
-                        _ = Vendor(user: result)
-                    }
-
-                    self.saveToPersistentStore()
-                    if self.token != nil {
-                        completion(nil)
-                    }
-                } catch {
-                    NSLog("Error decoding data/token: \(error)")
-                    completion(.badDecode)
-                    return
-                }
-            }.resume()
-        } else if let user = user as? ConsumerLogin {
-            let requestURL = baseURL.appendingPathComponent(user.username)
-
-            var urlComponents = URLComponents(url: requestURL, resolvingAgainstBaseURL: true)
-            let grantTypeQuery = URLQueryItem(name: "grant_type", value: "password")
-            let usernameQuery = URLQueryItem(name: "username", value: "\(user.username)")
-            let passwordQuery = URLQueryItem(name: "password", value: "\(user.password)")
-            urlComponents?.queryItems = [grantTypeQuery, usernameQuery, passwordQuery]
-
-            guard let url = urlComponents?.url else {
-                completion(.invalidInput)
-                return
-            }
-            var request = URLRequest(url: url)
-            request.httpMethod = HTTPMethod.post.rawValue
-            request.setValue("basic bGFtYmRhLWNsaWVudDpsYW1iZGEtc2VjcmV0", forHTTPHeaderField: "Authorization")
-            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
-
-            // request.httpBody = componentString?.data(using: .utf8)
-
-            let jsonEncoder = JSONEncoder()
-            do {
-                let userData = try jsonEncoder.encode(user)
-                request.httpBody = userData
-            } catch {
-                NSLog("Error encoding userData: \(error)")
-            }
-
-            URLSession.shared.dataTask(with: request) { (data, response, error) in
-                if let response = response as? HTTPURLResponse,
-                    response.statusCode != 200 {
-                    NSLog("Response status code is not 200. Status code: \(response.statusCode)")
-                }
-
-                if let error = error {
-                    NSLog("Error verifying user: \(error)")
-                    completion(.otherError(error))
-                    return
-                }
-
-                guard let data = data else {
-                    NSLog("No data returned from data task")
-                    completion(.noData)
-                    return
-                }
-
-                let jsonDecoder = JSONDecoder()
-                do {
-                    let result = try jsonDecoder.decode(ConsumerRepresentation.self, from: data)
-                    self.token = result.password
-                    self.user = user
-                    // TODO: Create new Background thread
-                    let context = CoreDataStack.shared.mainContext
-
-                    context.performAndWait {
-                        _ = Consumer(user: result)
-                    }
-
-                    self.saveToPersistentStore()
-                    if self.token != nil {
-                        completion(nil)
-                    }
-                } catch {
-                    NSLog("Error decoding data/token: \(error)")
-                    completion(.badDecode)
-                    return
-                }
-            }.resume()
-        }
-    }
-
-    func saveToPersistentStore() {
-        do {
-            try CoreDataStack.shared.mainContext.save()
-        } catch {
-            NSLog("Error saving managed object context: \(error)")
-        }
-    }
+	func saveToPersistentStore() {
+		do {
+			try CoreDataStack.shared.mainContext.save()
+		} catch {
+			NSLog("Error saving managed object context: \(error)")
+		}
+	}
 }
--- FoodTruck TrackR/FoodTruck TrackR/View Controllers/LoginViewController.swift
+++ FoodTruck TrackR/FoodTruck TrackR/View Controllers/LoginViewController.swift
@@ -9,79 +9,77 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    
-    @IBOutlet weak var loginSegmentedControl: UISegmentedControl!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var usernameTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var loginButton: UIButton!
-    var isVendor: Bool = false
-    let userController = UserController.shared
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        setupViews()
-    }
-    
-    private func setupViews() {
-        view.backgroundColor = .background
-        
-        passwordTextField.isSecureTextEntry = true
-        
-        loginSegmentedControl.backgroundColor = .text
-        
-        loginButton.backgroundColor = .text
-        loginButton.setTitleColor(UIColor.background, for: .normal)
-        loginButton.layer.cornerRadius = 8
-        
-        navigationController?.navigationBar.barStyle = .default
-        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.text]
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.text]
-    }
-    
-    private func logIn() {
-//        guard let username = usernameTextField.text,
-//              let password = passwordTextField.text else { return }
-//        if isVendor {
-//            userController.logIn(user: VendorLogin(username: username, password: password) { error in
-//                if error == nil {
-//                    self.dismiss(animated: true, completion: nil)
-//                }
-//            })
-//        } else {
-//            userController.logIn(user: ConsumerLogin(username: username, password: password) { error in
-//                if error == nil {
-//                    self.dismiss(animated: true, completion: nil)
-//                }
-//            })
-//        }
-    }
-    
-    @IBAction func loginTapped(_ sender: UIButton) {
-        logIn()
-    }
-    
-    @IBAction func segControlAction(_ sender: UISegmentedControl) {
-        switch loginSegmentedControl.selectedSegmentIndex {
-        case 0:
-            isVendor = false
-        case 1:
-            isVendor = true
-        default:
-            break
-        }
-    }
-    
-    
-    
-    /*
-    // MARK: - Navigation
+	@IBOutlet weak var loginSegmentedControl: UISegmentedControl!
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var usernameTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	@IBOutlet weak var loginButton: UIButton!
+	var isVendor: Bool = false
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	let vendorController = VendorController.shared
+	let consumerController = ConsumerController.shared
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupViews()
+	}
+
+	private func setupViews() {
+		view.backgroundColor = .background
+
+		passwordTextField.isSecureTextEntry = true
+
+		loginSegmentedControl.backgroundColor = .text
+
+		loginButton.backgroundColor = .text
+		loginButton.setTitleColor(UIColor.background, for: .normal)
+		loginButton.layer.cornerRadius = 8
+
+		navigationController?.navigationBar.barStyle = .default
+		navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.text]
+		navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.text]
+	}
+
+//	@IBAction func loginTapped(_ sender: UIButton) {
+//		guard let username = usernameTextField.text,
+//			let password = passwordTextField.text else { return }
+//		if isVendor {
+//			vendorController.logIn(user: VendorLogin(username: username, password: password) { error in
+//				if error == nil {
+//					self.dismiss(animated: true, completion: nil)
+//				}
+//			})
+//		} else {
+//			consumerController.logIn(user: ConsumerLogin(username: username, password: password) { error in
+//				if error == nil {
+//					self.dismiss(animated: true, completion: nil)
+//				}
+//			})
+//		}
+//	}
+
+	@IBAction func segControlAction(_ sender: UISegmentedControl) {
+		switch loginSegmentedControl.selectedSegmentIndex {
+		case 0:
+			isVendor = false
+		case 1:
+			isVendor = true
+		default:
+			break
+		}
+	}
+
+
+
+	/*
+	// MARK: - Navigation
+
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	// Get the new view controller using segue.destination.
+	// Pass the selected object to the new view controller.
+	}
+	*/
 }
