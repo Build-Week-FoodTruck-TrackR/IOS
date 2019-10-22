49bff54b8ca72c40306a4630294f65a04eae5213
brian <bfvilchez95@gmail.com>
2019-10-22 14:39:04 -0600

    fixed merge conflict

---
+++ FoodTruck TrackR/FoodTruck TrackR/Model Controllers/UserController.swift
@@ -0,0 +1,21 @@
+//
+//  UserController.swift
+//  FoodTruck TrackR
+//
+//  Created by Jordan Christensen on 10/19/19.
+//  Copyright © 2019 FoodTruckTrackR. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+class UserController {
+    
+}
+
