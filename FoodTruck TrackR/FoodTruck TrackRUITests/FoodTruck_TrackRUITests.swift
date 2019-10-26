e702edd4439c5a979dcd1ded534de35a2681ad40
Jordan Christensen <jordan.c4922@gmail.com>
2019-10-26 20:49:46 +0800

    Add more UITests

--- FoodTruck TrackR/FoodTruck TrackRUITests/FoodTruck_TrackRUITests.swift
+++ FoodTruck TrackR/FoodTruck TrackRUITests/FoodTruck_TrackRUITests.swift
@@ -39,19 +39,72 @@ class FoodTruck_TrackRUITests: XCTestCase {
         XCTAssertEqual(app.textFields["Settings.EmailTextField"].value as? String, "jordan@email.com")
     }
     
-    func testLogOut() {
+    func testLogOutAndLogBackIn() {
         testLogIn()
         XCTAssertNotNil(app.textFields["Username"].value as? String)
         
         app.staticTexts["Sign out"].tap()
         
-//        XCTAssertNil(ConsumerController.shared.user)
-//        XCTAssertNil(VendorController.shared.user)
+        let usernameTextField = app.textFields["Username"]
+        let passwordTextField = app.secureTextFields["Password"]
+        
+        usernameTextField.tap()
+        usernameTextField.typeText("brybry")
+        
+        passwordTextField.tap()
+        passwordTextField.typeText("123456")
+        
+        app.buttons["Log In"].tap()
+        
+        app.tabBars.buttons["gear"].tap()
+        
+        XCTAssertEqual(app.textFields["Settings.UsernameTextField"].value as? String, "brybry")
+        XCTAssertEqual(app.textFields["Settings.EmailTextField"].value as? String, "test@gmail.com")
     }
     
     func testSignUp() {
         app.staticTexts["Don't have an account yet? Sign up here!"].tap()
         
+        let email = app.textFields["Email"]
+        let username = app.textFields["Username"]
+        let password = app.secureTextFields["Password"]
         
+        let emailText = "testing@app.com"
+        let usernameText = "testingApp"
+        
+        email.tap()
+        email.typeText(emailText)
+        username.tap()
+        username.typeText(usernameText)
+        password.tap()
+        password.typeText("pass")
+        
+        app.buttons["Sign Up Here"].tap()
+        
+        app.tabBars.buttons["gear"].tap()
+        app.buttons["Settings.Eye"].tap()
+        
+        XCTAssertEqual(app.textFields["Settings.UsernameTextField"].value as? String, usernameText)
+        XCTAssertEqual(app.textFields["Settings.EmailTextField"].value as? String, emailText)
+    }
+    
+    func testSearch() {
+        let usernameTextField = app.textFields["Username"]
+        let passwordTextField = app.secureTextFields["Password"]
+        
+        usernameTextField.tap()
+        usernameTextField.typeText("jordan")
+        
+        passwordTextField.tap()
+        passwordTextField.typeText("pass")
+        
+        app.buttons["Log In"].tap()
+        
+        let searchBar = app.searchFields["Search Food Trucks"]
+        searchBar.tap()
+        searchBar.typeText("Ice\n")
+        
+        let firstCell = app.tables["searchResultsTable"].cells["FoodTruckCell0"]
+        XCTAssertTrue(firstCell.exists)
     }
 }
